7fc1577171595c011d17d184732f23837af1de83
Vincent Hu <vincent.guohua.hu@gmail.com>
2021-07-02 18:40:03 +0800

    7- fix integer overflow, and optimize, finally faster than 100% of c online submissions

--- 7_Reverse_Integer/reverse_integer.c
+++ 7_Reverse_Integer/reverse_integer.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 int reverse(int);
 
@@ -8,23 +9,18 @@ int main()
 	int b = reverse(-123);
 	int c = reverse(120);
 	int d = reverse(0);
+	int e = reverse(-2147483648);
 	printf("%d\n%d\n%d\n%d\n", a, b, c, d);
 }
 
 int reverse(int x){
-	// record whether current number is negative
-	int is_negative = x < 0;
+	long long res = 0;
 
-	int temp = x * (is_negative ? -1 : 1);
-	int res = 0;
-	while (temp/10 > 0)
-	{
-		res = res*10 + temp%10;
-		temp = temp/10;
+	while(x){
+		res = res*10 + x%10;
+		x /= 10;
 	}
-	res = res*10 + temp;
-
-	res = res * (is_negative ? -1 : 1);
-
+	if (res>pow(2, 31) || res<-pow(2, 31)-1)
+		return 0;
 	return res;
 }
