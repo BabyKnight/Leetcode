75442dd6e152e3b63222b60d024a4a5a68de0a3a
Vincent Hu <vincent.guohua.hu@gmail.com>
2021-08-24 14:21:49 +0800

    14: update C solution for problem 14, Runtime - 0ms, faster than 100% of C, Mem usage 5.8M, less than 72,78%

--- 14_Longest_Common_Prefix/longest_common_prefix.c
+++ 14_Longest_Common_Prefix/longest_common_prefix.c
@@ -10,25 +10,37 @@ int main(){
    char* test = longestCommonPrefix(str_1, 3);
    printf("%s\n", test);
    free(test);
+
+   char* str_2[3] = {"dog", "racecar", "car"};
+   char* test2 = longestCommonPrefix(str_2, 3);
+   printf("%s\n", test2);
+   free(test2);
+
+   char* str_3[1] = {"car"};
+   char* test3 = longestCommonPrefix(str_3, 1);
+   printf("%s\n", test3);
+   free(test3);
    return 0;
 }
 
 char * longestCommonPrefix(char ** strs, int strsSize){
 
-    char * res = (char*)malloc(sizeof(char)*20);
-    res[0] = '\0';
-    char temp;
+    char tmp;
+    int index=0;
 
-    for(int i=0; i<strlen(strs[0]); i++){
-        temp = *(strs[0]+i);
-        printf("%c\n", temp);
-    }
-
-    for(int i=0; i<strsSize; i++){
-        printf("%s\n", strs[i]);
-    }
-
-
+		for(index=0; index<strlen(*strs); index++){
+				for(int i=0; i<strsSize; i++){
+						printf("%d %c - %c\n",index, **(strs+i), *(*(strs+i)+index));
+						if(*(*strs+index) != *(*(strs+i)+index))
+								goto ret;
+				}
+		}
+		ret: ;
+		printf("index is [%d]\n", index);
+    char * res = (char*)malloc(sizeof(char)*(index+1));
+		// set the last char to \0
+    res[index] = '\0';
+    strncpy(res, *strs, index);
     return res;
 
 
