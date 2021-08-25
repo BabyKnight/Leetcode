fd03316fd4a800c92b5fe55f60f07c3122f70ca0
Vincent Hu <vincent.guohua.hu@gmail.com>
2021-08-25 20:00:49 +0800

    21: add python3 solution

---
+++ 21_Merge_Two_Sorted_Lists/merge_2_sorted_lists.py
@@ -0,0 +1,73 @@
+#/us/bin/env python3
+
+class ListNode:
+		def __init__(self, val=0, next=None):
+				self.val = val
+				self.next = next
+
+class Solution:
+		def mergeTwoLists(self, l1, l2):
+				p = l1
+				q = l2
+
+				if l1 is None:
+						return l2;
+				if l2 is None:
+						return l1;
+
+				if p.val <= q.val:
+						res = p
+						p = p.next
+				else:
+						res = q
+						q = q.next
+				curr = res
+
+				while p is not None and q is not None:
+						if p.val <= q.val:
+								curr.next = p
+								curr = curr.next
+								p = p.next
+						else:
+								curr.next = q
+								curr = curr.next
+								q = q.next
+				if p is not None:
+						curr.next = p
+				if q is not None:
+						curr.next = q
+
+				return res
+						
+
+def create_singly_linked_list(l):
+		curr = ListNode(val=l[0])
+		res = curr
+
+		print(l[1:])
+		for v in l[1:]:
+				l = ListNode(val=v)
+				curr.next = l
+				curr = l
+		return res
+
+def print_list(l):
+		curr = l
+		while curr is not None:
+				print(curr.val)
+				curr = curr.next
+
+
+if __name__ == '__main__':
+
+		a = [1, 3, 5, 7, 9]
+		b = [0, 2, 4, 6, 8, 10]
+
+		la = create_singly_linked_list(a)
+		print_list(la)
+		lb = create_singly_linked_list(b)
+		print_list(lb)
+
+		s = Solution()
+		res = s.mergeTwoLists(la, lb)
+		print_list(res)
